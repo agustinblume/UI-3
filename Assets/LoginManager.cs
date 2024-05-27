9beac0f5553095603018cba65b27ee39c5047e0c
agustinblume <agustinblume@gmail.com>
2024-05-27 15:56:34 -0300

    Hecho

--- Assets/LoginManager.cs
+++ Assets/LoginManager.cs
@@ -1,18 +1,36 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LoginManager : MonoBehaviour
 {
+    public InputField InputNombre;
+    public Text nombre;
+    string contra = "Berni";
     // Start is called before the first frame update
     void Start()
     {
-        
+       
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    public void Consola()
+    {
+        string ContraUsuario = InputNombre.text;
+        if (ContraUsuario == contra)
+        {
+            nombre.text = "correcto";
+            Debug.Log("Acceso correcto");
+        }
+        else
+        {
+            Debug.Log("Acceso denegado");
+        }
+
     }
 }
